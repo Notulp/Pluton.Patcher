fffad64f770627b284e39819d5d5afd23a4c84b8
balu92 <balugyanszky.gabor@gmail.com>
2015-12-10 16:00:13 +0000

    added fieldpatcher

---
+++ Reflection/FieldPatcher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace Pluton.Patcher.Reflection
+{
+    public class FieldPatcher : PatcherObject
+    {
+        internal FieldDefinition fieldDefinition;
+
+        public bool Public {
+            get {
+                return fieldDefinition.IsPublic;
+            }
+            set {
+                fieldDefinition.IsPublic = value;
+                fieldDefinition.IsPrivate = !value;
+            }
+        }
+
+        public bool Static {
+            get {
+                return fieldDefinition.IsStatic;
+            }
+            set {
+                fieldDefinition.IsStatic = value;
+            }
+        }
+
+        public bool ReadOnly {
+            get {
+                return fieldDefinition.IsInitOnly;
+            }
+            set {
+                fieldDefinition.IsInitOnly = value;
+            }
+        }
+
+        public FieldPatcher(PatcherObject prnt, FieldDefinition fieldDef) : base(prnt)
+        {
+            fieldDefinition = fieldDef;
+        }
+
+        public void SetPublic(bool value = true)
+        {
+            Public = value;
+        }
+
+        public void SetStatic(bool value = true)
+        {
+            Static = value;
+        }
+
+        public void SetReadOnly(bool value = true)
+        {
+            ReadOnly = value;
+        }
+
+        public void SetConstant(object value = null)
+        {
+            fieldDefinition.HasConstant = true;
+            fieldDefinition.Constant = value;
+        }
+    }
+}
+
